0d9a8589b81914fa78b4c00464486bc66628fae9
grzegorz.pasterny <grzegorz.pasterny@EUCTPL1NB018>
2020-10-20 09:01:18 +0200

    now it is working as expected.

--- Cezar_cipher/Program.fs
+++ Cezar_cipher/Program.fs
@@ -4,31 +4,22 @@ open System
 
 
 let CezarCipher =
+    Console.WriteLine("Welcome in Cezar Cipher encrypter.")
     
     let getText = 
         printfn "Put here text you would like to process:"
-        "Ala ma kota".ToLower()
-        //Console.ReadLine().ToLower()
+        //"Ala ma kota".Trim().ToLower()
+        Console.ReadLine().Replace(" ","").ToLower()
 
     let getShift = 
         printfn "What is the shift of the cipher?"
-        "10"
-        //Console.ReadLine()
+        //"10"
+        Console.ReadLine()
         |> int
     
-    //let moveChar (input : char, shift : int, direction : bool) = 
-        
-    //    let NumericChar = int input
+    let getShiftMod = 
+        getShift % 26
 
-    //    match direction with
-    //    | true -> match NumericChar with
-    //              | i when i >= 68 && i < 95 -> 71
-    //              | _ -> 70
-            
-    //    | false -> match NumericChar with
-    //              | i when i >= 68 && i < 95 -> 71
-    //              | _ -> 70
-        
     let moveString (input : string) shift =
         
         input.ToCharArray()
@@ -36,16 +27,13 @@ let CezarCipher =
         |> List.map (fun c -> 
             match int c with
                     | i when i + shift > 122 -> i - 26 + shift
-                    | i when i + shift < 122 -> i + shift
-                    | i when i - shift < 97 -> i + 26 - shift
-                    | i when i - shift > 97 -> i - shift
-                )
+                    | i when i + shift < 122 && i + shift > 97 -> i + shift
+                    | i when i + shift < 97 -> i + 26 + shift
+                    )
         |> List.map (fun i -> char i)
         |> String.Concat
-        //[for c in input do yield (moveChar c shift direction)]
 
-    printfn "Welcome in Cezar Cipher encrypter." 
-    Console.WriteLine(moveString getText getShift)
+    Console.WriteLine(moveString getText getShiftMod)
 
 [<EntryPoint>]
 CezarCipher
